908747ea26c0b3073c2cf28c9d444033eccc644b
Kee Pan <omega.ult@gmail.com>
2014-08-05 19:57:01 +0800

    Just a beginning

--- CODDatabaseManager.py
+++ CODDatabaseManager.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import sys
 import sqlite3
 
 sqlite3.connect(".\\cod.db")
+
+
+class CoDDatabaseManager(object):
+	"""
+	Initialize database using dbLoc as fullpath of .db file.
+	"""
+	def __init__(self, dbLoc):
+		print "yooooo"
